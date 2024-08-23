cf58330525c4bee9480e134b8827ca21440c0f25
samettopcu88 <samet.topcu.88@icloud.com>
2024-08-24 01:33:21 +0300

    Repository klasörü ve Extensions klasörü oluşturuldu.

---
+++ DAL/Extensions/DataLayerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using DAL.Context;
+using DAL.Repositories.Abstractions;
+using DAL.Repositories.Concretes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Extensions
+{
+    public static class DataLayerExtensions
+    {
+        public static IServiceCollection LoadDataLayerExtension(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+    }
+}
--- WEB/Program.cs
+++ WEB/Program.cs
@@ -1,7 +1,10 @@
 using DAL.Context;
+using DAL.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.LoadDataLayerExtension(builder.Configuration);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
