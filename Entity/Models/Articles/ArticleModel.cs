8c6c52cc8445d876f2ea34efb645b05dddf4c4a4
samettopcu88 <samet.topcu.88@icloud.com>
2024-08-24 17:14:24 +0300

    Admin paneli düzenlemeleri yapıldı.

--- Entity/Models/Articles/ArticleModel.cs
+++ Entity/Models/Articles/ArticleModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ENTITY.Entities;
+using ENTITY.Models.Cetegories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ SERVICE/AutoMapper/Categories/CategoryProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ENTITY.Entities;
+using ENTITY.Models.Cetegories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERVICE.AutoMapper.Categories
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<CategoryModel, Category>().ReverseMap();
+            CreateMap<CategoryAddModel, Category>().ReverseMap();
+            CreateMap<CategoryUpdateModel, Category>().ReverseMap();
+        }
+    }
+}
